68f538e0f79522ff562352935043542f1b10dffc
BCimino <bcimino@cccis.com>
2024-09-16 10:48:06 -0500

    Added `History`

---
+++ FightinWords.Tests/HistoryTests.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using FightinWords.Submissions;
+
+namespace FightinWords;
+
+public class HistoryTests
+{
+    [Test]
+    public void History_Record([Values(1,2,3)] int capacity)
+    {
+        Debug.Assert(capacity > 0);
+
+        var history = new History<string>(capacity);
+
+        var alphabet = Enumerable.Range('a', 'z' - 'a' + 1)
+                                 .Select(it => (char)it)
+                                 .Select(it => it.ToString());
+
+        foreach (var letter in alphabet)
+        {
+            RecordAndAssert(history, letter, $"+ {letter}");
+        }
+    }
+
+    private static void RecordAndAssert_WhenMoreCapacityAvailable<T>(History<T> history, T next, string label)
+    {
+        Assert.That(history.Count, Is.LessThan(history.Capacity));
+        var expectedNextContents = ToArrayByEnumerator(history).Prepend(next).ToImmutableArray();
+
+        TestHelpers.AssertEquals(history.Record(next, out var evictedEntry), false);
+        TestHelpers.AssertEquals(evictedEntry, default(T));
+        TestHelpers.AssertEquals(history.OffsetInStorage,            0);
+
+        Assert_Content(history, expectedNextContents, 0, label);
+    }
+
+    private static void RecordAndAssert_WhenAtCapacity<T>(History<T> history, T next, string label)
+    {
+        TestHelpers.AssertEquals(history.Count, history.Capacity);
+        
+        var expectedOffsetInStorage = (history.OffsetInStorage + 1) % history.Capacity;
+        var expectedNextContents    = ToArrayByEnumerator(history).Prepend(next).SkipLast(1).ToImmutableArray();
+        var expectedToBeEvicted     = history.Oldest;
+        
+        Assert.Multiple(() =>
+        {
+            TestHelpers.AssertEquals(history.Record(next, out var evictedEntry), true);
+            TestHelpers.AssertEquals(evictedEntry,                               expectedToBeEvicted);
+            Assert_Content(history, expectedNextContents, expectedOffsetInStorage, label);
+        });
+    }
+
+    private static void RecordAndAssert<T>(History<T> history, T next, string label)
+    {
+        if (history.Capacity > history.Count)
+        {
+            RecordAndAssert_WhenMoreCapacityAvailable(history, next, label);
+        }
+        else
+        {
+            RecordAndAssert_WhenAtCapacity(history, next, label);
+        }
+    }
+
+    [Test]
+    public void Newest_Oldest()
+    {
+        var history = new History<string>(3);
+        history.Record("grandparent");
+        TestHelpers.AssertEquals(history.OffsetInStorage, 0);
+        history.Record("parent");
+        TestHelpers.AssertEquals(history.OffsetInStorage, 0);
+        history.Record("child");
+        TestHelpers.AssertEquals(history.OffsetInStorage, 0);
+        
+        Assert.Multiple(() =>
+        {
+            TestHelpers.AssertEquals(history.Oldest, "grandparent");
+            TestHelpers.AssertEquals(history.Newest, "child");
+        });
+    }
+    
+    [Test]
+    public void History_Record_Manual()
+    {
+        var history = new History<string>(3);
+        Assert_Content(history, [], 0, "initial");
+
+        history.Record("a");
+        Assert_Content(history, ["a"], 0, "+ a");
+
+        history.Record("b");
+        Assert_Content(history, ["b", "a"], 0, "+ b");
+
+        history.Record("c");
+        Assert_Content(history, ["c", "b", "a"], 0, "+ c");
+
+        history.Record("d");
+        Assert_Content(history, ["d", "c", "b"], 1, "+ d");
+
+        history.Record("e");
+        Assert_Content(history, ["e", "d", "c"], 2, "+ e");
+
+        history.Record("f");
+        Assert_Content(history, ["f", "e", "d"], 0, "+ f");
+
+        history.Record("g");
+        Assert_Content(history, ["g", "f", "e"], 1, "+ g");
+
+        history.Record("h");
+        Assert_Content(history, ["h", "g", "f"], 2, "+ h");
+    }
+
+    [SuppressMessage("ReSharper", "ForCanBeConvertedToForeach")]
+    private static ImmutableArray<T> ToArrayByIndices<T>(History<T> history)
+    {
+        var builder = ImmutableArray.CreateBuilder<T>(history.Count);
+        for (int i = 0; i < history.Count; i++)
+        {
+            builder.Add(history[i]);
+        }
+
+        return builder.DrainToImmutable();
+    }
+
+    private static ImmutableArray<T> ToArrayByEnumerator<T>(History<T> history)
+    {
+        var builder = ImmutableArray.CreateBuilder<T>(history.Count);
+
+        var erator = history.GetEnumerator();
+
+        while (erator.MoveNext())
+        {
+            builder.Add(erator.Current);
+        }
+
+        return builder.DrainToImmutable();
+    }
+
+    [StackTraceHidden]
+    private static void Assert_Content<T>(
+        History<T> actual,
+        IList<T>    expected,
+        int expectedOffsetFromStorage,
+        string label
+    )
+    {
+        Assert.Multiple([StackTraceHidden]() =>
+        {
+            TestHelpers.AssertEquals(actual.Count, expected.Count, label);
+            TestHelpers.AssertEquals(actual.OffsetInStorage, expectedOffsetFromStorage, label);
+
+            TestHelpers.AssertEquals(ToArrayByIndices(actual),    expected, label);
+            TestHelpers.AssertEquals(ToArrayByEnumerator(actual), expected, label);
+
+            if (expected.Count > 0)
+            {
+                TestHelpers.AssertEquals(actual.Newest, expected[0], label);
+                TestHelpers.AssertEquals(actual.Oldest, expected[^1], label);
+            }
+
+            Assert_CannotGetIndicesInStorageButNotHistory(actual);
+        });
+    }
+
+    private static void Assert_CannotGetIndicesInStorageButNotHistory<T>(History<T> actual)
+    {
+        foreach (var i in Enumerable.Range(actual.Count, actual.Capacity - actual.Count))
+        {
+            Assert.That(() => actual[i], Throws.TypeOf<IndexOutOfRangeException>());
+        }
+                  
+    }
+
+    [Test]
+    public void OneSeater()
+    {
+        var history = new History<string>(1);
+
+        history.Record("first");
+        history.Record("second");
+        
+        Assert_Content(history, ["second"], 0, "?");
+    }
+
+    [Test]
+    public void TwoSeater()
+    {
+        var history = new History<string>(2);
+
+        history.Record("grandparent");
+        history.Record("parent");
+        history.Record("child");
+        
+        Assert_Content(history, ["child", "parent"], 1, "after child kicks out grandparent");
+    }
+
+    [Test]
+    public void ThreeSeater()
+    {
+        var history = new History<string>(3);
+
+        history.Record("great-grandparent");
+        history.Record("grandparent");
+        history.Record("parent");
+        
+        RecordAndAssert_WhenAtCapacity(history, "child", "child kicking out great-grandparent");
+        
+        Assert_Content(history, ["child","parent","grandparent"], 1, "after child kicks out great-grandparent");
+
+        RecordAndAssert_WhenAtCapacity(history, "future-baby", "future-baby kicking out grandparent");
+        Assert_Content(history, ["future-baby", "child", "parent"], 2, "after future-baby kicks out grandparent");
+    }
+    
+    [TestCase(1, 0, 0)]
+    [TestCase(1, 1, 0)]
+    [TestCase(1, 2, 0)]
+    [TestCase(1, 3, 0)]
+    
+    [TestCase(3, -6, 0)]
+    [TestCase(3, -5, 1)]
+    [TestCase(3, -4, 2)]
+    [TestCase(3, -3, 0)]
+    [TestCase(3, -2, 1)]
+    [TestCase(3, -1, 2)]
+    [TestCase(3, 0, 0)]
+    [TestCase(3, 1, 1)]
+    [TestCase(3, 2, 2)]
+    [TestCase(3, 3, 0)]
+    [TestCase(3, 4, 1)]
+    [TestCase(3, 5, 2)]
+    [TestCase(3, 6, 0)]
+    public void LoopIndex(int length, int index, int expectedLoopedIndex)
+    {
+        TestHelpers.AssertEquals(History<int>.LoopIndex(index, length), expectedLoopedIndex);
+    }
+}
---
+++ FightinWords/Submissions/History.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FightinWords.Submissions;
+
+/// <summary>
+/// Sort of like a <see cref="Stack{T}"/>, in that it is last-in first-out, but has a limited <see cref="Capacity"/>,
+/// automatically removing the oldest entry when it runs out of space.
+/// </summary>
+/// <typeparam name="T">the element type</typeparam>
+public sealed class History<T>(int capacity) : IEnumerable<T>
+{
+    private readonly T[] _storage = new T[capacity];
+
+    private int _offsetInStorage;
+
+    internal int OffsetInStorage
+    {
+        get => _offsetInStorage;
+        private set => _offsetInStorage = value % Capacity;
+    }
+
+    /// <summary>
+    /// The maximum number of entries I can <see cref="Record(T)"/> before I have to start forgetting old ones.
+    /// </summary>
+    public int Capacity => _storage.Length;
+
+    /// <summary>
+    /// The number of entries that I <i>currently</i> have.
+    /// </summary>
+    public int Count { get; private set; }
+
+    private int NewestIndexInStorage => GetIndexInStorage(0);
+    private int OldestIndexInStorage => OffsetInStorage;
+
+    /// <summary>
+    /// My most recently <see cref="Record(T)"/>ed entry.
+    /// </summary>
+    public T Newest => _storage[NewestIndexInStorage];
+    /// <summary>
+    /// The first <see cref="Record(T)"/>ed entry <i>(that I can remember)</i>. 
+    /// </summary>
+    public T Oldest => _storage[OldestIndexInStorage];
+
+    /// <summary>
+    /// Returns my <paramref name="index"/>-th most recently <see cref="Record(T)"/>ed <typeparamref name="T"/> entry. 
+    /// </summary>
+    /// <param name="index"></param>
+    /// <exception cref="IndexOutOfRangeException">if <paramref name="index"/> isn't within <c>0..</c><see cref="Count"/></exception>
+    public T this[int index]
+    {
+        get
+        {
+            // We have to put an extra check here, because `GetIndexInStorage(index)` will constrain the `index` to be within `length`.
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return _storage[GetIndexInStorage(index)];
+        }
+    }
+
+    private int GetIndexInStorage(int indexInHistory) =>
+        LoopIndex(OffsetInStorage + Count - 1 - indexInHistory, Capacity);
+
+    /// <summary>
+    /// Takes <b><i>any</i></b> <see cref="int"/>, positive or negative, and Pac-Man loops it to be within the indices of a collection with <paramref name="length"/> elements.
+    /// </summary>
+    /// <example>
+    /// <code><![CDATA[
+    /// LoopIndex(-4, 3); // => 1
+    /// LoopIndex(-3, 3); // => 0
+    /// LoopIndex(-2, 3); // => 1
+    /// LoopIndex(-1, 3); // => 2
+    ///  
+    /// LoopIndex( 0, 3); // => 0
+    ///  
+    /// LoopIndex( 1, 3); // => 1
+    /// LoopIndex( 2, 3); // => 2
+    /// LoopIndex( 3, 3); // => 0
+    /// LoopIndex( 4, 3); // => 1
+    /// LoopIndex( 5, 3); // => 2
+    /// LoopIndex( 6, 3); // => 0
+    ///  
+    /// LoopIndex(int.MaxValue, 3); // => 1
+    /// ]]></code>
+    /// </example>
+    /// <param name="index">the index that you want to loop around to keep inside of <paramref name="length"/></param>
+    /// <param name="length">the size of the actual collection</param>
+    /// <returns>a value in <c>0..length</c></returns>
+    internal static int LoopIndex(int index, int length)
+    {
+        Debug.Assert(length > 0,
+            "Can't loop an index through a collection with length 0, because that would be INFINITE");
+
+        index %= length;
+        if (index < 0)
+        {
+            return length - ~index - 1;
+        }
+
+        return index;
+    }
+
+    /// <summary>
+    /// Records a new <see cref="History{T}"/> entry.
+    /// <br/>
+    /// The new <paramref name="entry"/> will become my <see cref="Newest"/> element, at index <c>0</c>.
+    /// <br/>
+    /// If I'm already at <see cref="Capacity"/>, then my <see cref="Oldest"/> entry will be <paramref name="evicted"/>.
+    /// </summary>
+    /// <param name="entry">the new <see cref="Newest"/> entry</param>
+    /// <param name="evicted">the entry that got removed, if I was already at <see cref="Capacity"/></param>
+    /// <returns><c>true</c> if I was already at <see cref="Capacity"/> and someone got <paramref name="evicted"/></returns>
+    public bool Record(T entry, [MaybeNullWhen(false)] out T evicted)
+    {
+        if (Count == Capacity)
+        {
+            evicted                        = Oldest;
+            _storage[OldestIndexInStorage] = entry;
+            OffsetInStorage += 1;
+            return true;
+        }
+
+        evicted                        =  default;
+        _storage[NewestIndexInStorage] =  entry;
+        Count                          += 1;
+        return false;
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="Record(T,out T)"/>
+    /// </summary>
+    /// <param name="entry"><inheritdoc cref="Record(T,out T)"/></param>
+    public void Record(T entry) => Record(entry, out _);
+
+    /// <summary>
+    /// Enumerates through my entries <b><i>from <see cref="History{T}.Newest"/> to <see cref="History{T}.Oldest"/></i></b>.
+    /// </summary>
+    /// <remarks>
+    /// If my <see cref="History{T}"/> gets modified while the <see cref="Enumerator"/> is in use, you're gunna have a bad time.
+    /// If you want to be extra safe, make an immutable snapshot of the <see cref="History{T}"/> using <see cref="ImmutableArray.ToImmutableArray{TSource}(System.Collections.Generic.IEnumerable{TSource})"/>.
+    /// <br/>
+    /// <br/>
+    /// 📎 Note that this behavior matches that of <see cref="ConcurrentDictionary{TKey,TValue}"/>'s <see cref="ConcurrentDictionary{TKey,TValue}.GetEnumerator"/>, etc.
+    /// </remarks>
+    public Enumerator GetEnumerator() => new(this);
+
+    /// <remarks>
+    /// Interestingly, the javadocs for <see cref="ConcurrentStack{T}"/>.<see cref="ConcurrentStack{T}.GetEnumerator"/> note that the usage of <c>yield return</c>
+    /// causes their generated <see cref="IEnumerator{T}"/> to contain a "snapshot" of the contents.
+    /// This method does not do that.
+    /// <br/>
+    /// I prefer it that way, so that it matches the behavior of the <c>ref struct</c> <see cref="GetEnumerator"/>,
+    /// but I wonder what's actually causing <see cref="ConcurrentStack{T}.GetEnumerator"/> to be a snapshot?
+    /// </remarks>
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<T>)this).GetEnumerator();
+
+    /// <inheritdoc cref="History{T}.GetEnumerator"/>
+    public struct Enumerator(History<T> history)
+    {
+        private int _position = -1;
+        public  T   Current => history[_position];
+
+        public bool MoveNext()
+        {
+            _position += 1;
+            return _position < history.Count;
+        }
+    }
+
+    /// <summary>
+    /// <see cref="Array.Clear(System.Array)"/>s my <see cref="_storage"/> and resets my <see cref="Count"/>.
+    /// </summary>
+    public void Clear()
+    {
+        Array.Clear(_storage);
+        OffsetInStorage = 0;
+        Count           = 0;
+    }
+}
