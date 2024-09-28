6826e49123a146ad4a77673c305922bfa7a815ef
BCimino <bcimino@cccis.com>
2024-09-27 20:05:34 -0500

    Added SharedResources.cs

---
+++ FightinWords.Console/GamePlan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+using FightinWords.WordLookup;
+
+namespace FightinWords.Console;
+
+/// <summary>
+/// The stuff that needs to get configured before we can start playing, and shouldn't change while in-game.
+/// </summary>
+public record GamePlan
+{
+    public required Language             Language          { get; init; }
+    public required Word                 ProgenitorPool    { get; init; }
+    public required TimeSpan             TimeLimit         { get; init; }
+    public          SpectreFactory.Theme Theme             { get; init; } = new();
+    public          int                  MinimumWordLength { get; init; } = 3;
+}
---
+++ FightinWords.Console/SharedResources.cs
@@ -0,0 +1,18 @@
+﻿using FightinWords.Submissions;
+using Spectre.Console;
+
+namespace FightinWords.Console;
+
+/// <summary>
+/// Stuff that different "components" <i>(<see cref="LetterPoolDisplay"/>, <see cref="SubmissionManager"/>, etc.)</i>
+/// should be sharing, even though they don't know it, such as a <see cref="Random"/> number generator.
+/// </summary>
+/// <remarks>
+/// Note that these things are expected to be "read-only" - the purpose of this class is <b>coordination</b>, not communication. 
+/// </remarks>
+public sealed class SharedResources
+{
+    public required GamePlan             GamePlan { get; init; }
+    public required Random               Random   { get; init; }
+    public required IAnsiConsole         Console  { get; init; }
+}
