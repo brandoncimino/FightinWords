d818d9923ac520b2e4397d243567c6ed867ee20b
BCimino <bcimino@cccis.com>
2024-10-01 09:20:50 -0500

    added screening

---
+++ FightinWords/Submissions/ISubmissionScreener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+using OneOf;
+
+namespace FightinWords.Submissions;
+
+/// <summary>
+/// Checks if a given <typeparamref name="INPUT"/> is even <see cref="Legality.Legal"/>.
+///
+/// This is in contrast to <see cref="WordLookup.IWordLookup"/>, which checks if a <i><see cref="Legality.Legal"/></i> string is also a word.
+/// </summary>
+public interface ISubmissionScreener<in INPUT, SUCCESS, FAILURE>
+{
+    public OneOf<SUCCESS, FAILURE> ScreenInput(INPUT rawSubmission);
+}
---
+++ FightinWords/Submissions/LetterPoolScreener.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Immutable;
+using OneOf;
+using OneOf.Types;
+
+namespace FightinWords.Submissions;
+
+public sealed class LetterPoolSubmissionScreener : ISubmissionScreener<string, Word, LetterPoolSubmissionScreener.FailedScreening>
+{
+    public required Word LetterPool        { get; init; }
+    public required int  MinimumWordLength { get; init; }
+
+    public OneOf<Word, FailedScreening> ScreenInput(string rawInput)
+    {
+        string userInput = rawInput;
+        userInput = userInput.ToLower();
+
+        if (Word.TryParse(userInput, out var result,
+                StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries) == false)
+        {
+            return new FailedScreening("You entered some weird junk that we couldn't parse.");
+        }
+
+        if (result.Length < MinimumWordLength)
+        {
+            return new FailedScreening($"You must enter at least {MinimumWordLength} letters.");
+        }
+
+        var remainingPool = LetterPool.ToList();
+        // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
+        foreach (var letter in result)
+        {
+            if (remainingPool.Remove(letter) == false)
+            {
+                return new FailedScreening(LetterPool.Contains(letter)
+                    ? $"You've exceeded your `{letter}` budget."
+                    : $"The letter `{letter}` isn't in your pool.");
+            }
+        }
+
+        return result;
+    }
+
+    public record FailedScreening(string Reason);
+}
