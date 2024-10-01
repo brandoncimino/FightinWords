be0a1af513189b6c652f0ce95f68ace8779e6747
BCimino <bcimino@cccis.com>
2024-10-01 09:18:28 -0500

    Added `ConsoleHelpers`

---
+++ FightinWords.Console/ConsoleHelpers.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace FightinWords.Console;
+
+public static class ConsoleHelpers
+{
+    [MustUseReturnValue]
+    public static string ReadNonEmptyInput()
+    {
+        string? userInput         = default;
+        var (beforeLeft, beforeTop) = System.Console.GetCursorPosition();
+        while (string.IsNullOrEmpty(userInput))
+        {
+            System.Console.SetCursorPosition(beforeLeft, beforeTop);
+            userInput = System.Console.ReadLine();
+        }
+
+        return userInput;
+    }
+}
