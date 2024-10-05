83949f5f6e6182738678584c9718305f5ace866f
BCimino <bcimino@cccis.com>
2024-10-05 00:33:52 -0500

    Small javadoc update

--- FightinWords.Console/SharedResources.cs
+++ FightinWords.Console/SharedResources.cs
@@ -4,7 +4,7 @@ using Spectre.Console;
 namespace FightinWords.Console;
 
 /// <summary>
-/// Stuff that different "components" <i>(<see cref="LetterPoolDisplay"/>, <see cref="SubmissionManager"/>, etc.)</i>
+/// Stuff that, <i><b>during a game</b></i>, different "components" <i>(<see cref="LetterPoolDisplay"/>, <see cref="SubmissionManager"/>, etc.)</i>
 /// should be sharing, even though they don't know it, such as a <see cref="Random"/> number generator.
 /// </summary>
 /// <remarks>
@@ -12,7 +12,7 @@ namespace FightinWords.Console;
 /// </remarks>
 public sealed class SharedResources
 {
-    public required GamePlan             GamePlan { get; init; }
-    public required Random               Random   { get; init; }
-    public required IAnsiConsole         Console  { get; init; }
+    public required GamePlan     GamePlan { get; init; }
+    public required Random       Random   { get; init; }
+    public required IAnsiConsole Console  { get; init; }
 }
