54e5365618021423b8b20861b1bcc39642fde11b
BCimino <bcimino@cccis.com>
2025-01-26 22:22:51 -0600

    added `TileBag`

---
+++ FightinWords.Console/LetterPools/TileBag.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace FightinWords.Console.LetterPools;
+
+/// <summary>
+/// Contains a bunch of <see cref="T"/>s where each <see cref="T"/> can be duplicated.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class TileBag<T> where T : notnull
+{
+    private readonly Dictionary<T, int> _tileCounts;
+    public           int                TotalTiles { get; private set; }
+
+    /// <returns>an <b>immutable snapshot</b> of my <see cref="_tileCounts"/></returns>
+    public ImmutableDictionary<T, int> GetTileCounts() => _tileCounts.ToImmutableDictionary();
+
+    public TileBag(IDictionary<T, int> tileCounts)
+    {
+        Preconditions.Require(tileCounts.Values.All(it => it >= 0),
+            _condition: "You cannot have a negative number of tiles!");
+
+        this._tileCounts = tileCounts
+                           .Where(kvp => kvp.Value >= 0)
+                           .ToDictionary();
+        this.TotalTiles = _tileCounts.Values.Sum();
+    }
+
+    private void ThrowIfEmpty()
+    {
+        if (TotalTiles <= 0)
+        {
+            throw new InvalidOperationException("Can't draw a tile from an empty bag!");
+        }
+    }
+
+    private T DrawTile(int tileIndex)
+    {
+        ThrowIfEmpty();
+        Preconditions.Require(tileIndex >= 0 && tileIndex < TotalTiles);
+
+        var tile = GetKeyByWeight(_tileCounts, tileIndex);
+        _tileCounts[tile] -= 1;
+        TotalTiles        -= 1;
+        return tile;
+    }
+
+    public T DrawOne(Random random)
+    {
+        return DrawTile(random.Next(TotalTiles));
+    }
+
+    public ValueArray<T> DrawX(Random random, [NonNegativeValue] int drawAmount)
+    {
+        if (drawAmount == 0)
+        {
+            return ValueArray<T>.Empty;
+        }
+
+        Preconditions.Require(drawAmount <= TotalTiles, true,
+            $"Cannot draw {drawAmount} tiles because I only contain {TotalTiles}!");
+
+        var arrayBuilder = ImmutableArray.CreateBuilder<T>(drawAmount);
+        for (int i = 0; i < drawAmount; i++)
+        {
+            arrayBuilder.Add(DrawOne(random));
+        }
+
+        return arrayBuilder.MoveToImmutable();
+    }
+
+    private static K GetKeyByWeight<K>(IDictionary<K, int> weightedBag, int weight)
+    {
+        Debug.Assert(weight >= 0);
+        var weightLeft = weight;
+
+        foreach (var (k, w) in weightedBag)
+        {
+            if (weightLeft < w)
+            {
+                return k;
+            }
+
+            weightLeft -= w;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(weight), weight,
+            $"Greater than the total weight in the bag, {weightedBag.Values.Sum()}!");
+    }
+}
---
+++ FightinWords.Tests/LetterPools/TileBagTests.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using FightinWords.Console.LetterPools;
+
+namespace FightinWords.LetterPools;
+
+public class TileBagTests
+{
+    [Test]
+    public void EmptyBagTest()
+    {
+        var emptyBag = new TileBag<char>(ImmutableDictionary<char, int>.Empty);
+
+        TestHelpers.AssertEquals(emptyBag.TotalTiles, 0);
+        Assert.Throws<InvalidOperationException>(() => emptyBag.DrawOne(Random.Shared));
+        Assert.Throws<ArgumentException>(() => emptyBag.DrawX(Random.Shared, 1));
+    }
+
+    private static ImmutableDictionary<char, int> ValidTileCounts => ImmutableDictionary.CreateRange(
+        new Dictionary<char, int>()
+        {
+            ['a'] = 0,
+            ['b'] = 1,
+            ['c'] = 5,
+        });
+
+    public static IEnumerable<ImmutableDictionary<char, int>> AllValidTileCounts() => [ValidTileCounts];
+
+    #region Helpers
+
+    private static TileBag<T> CreateBag<T>(IDictionary<T, int> tileCounts)
+    {
+        var expectedTotalTiles = tileCounts.Values.Sum();
+        var tileBag            = new TileBag<T>(tileCounts);
+        TestHelpers.AssertEquals(tileBag.TotalTiles, expectedTotalTiles);
+        return tileBag;
+    }
+
+    private static ValueArray<T> FlattenTiles<T>(IDictionary<T, int> tileCounts)
+    {
+        var flattened = tileCounts
+                        .SelectMany(kvp => Enumerable.Repeat(kvp.Key, kvp.Value))
+                        .ToValueArray();
+
+        Debug.Assert(flattened.Count() == tileCounts.Values.Sum());
+
+        return flattened;
+    }
+
+    #endregion
+
+    [Test]
+    public void DrawX(
+        [ValueSource(nameof(AllValidTileCounts))] ImmutableDictionary<char, int> tileCounts,
+        [Values(2)]                               int                            x
+    )
+    {
+        var tileBag                = CreateBag(tileCounts);
+        var expectedTotalTiles     = tileCounts.Values.Sum();
+        var expectedFlattenedTiles = FlattenTiles(tileCounts);
+
+        Preconditions.Require(expectedTotalTiles >= x);
+        TestHelpers.AssertEquals(tileBag.TotalTiles, expectedTotalTiles);
+
+        var drawn = tileBag.DrawX(Random.Shared, x);
+        TestHelpers.AssertEquals(tileBag.TotalTiles, expectedTotalTiles - x);
+        TestHelpers.AssertEquals(drawn.Count(),      x);
+
+        var expectedAfterDraw = expectedFlattenedTiles.Values.RemoveRange(drawn).ToValueArray();
+
+        var actualAfterDraw = tileBag.GetTileCounts();
+        TestHelpers.AssertEquals(FlattenTiles(actualAfterDraw), expectedAfterDraw);
+    }
+
+    [Test]
+    public void DrawX_AllTiles([ValueSource(nameof(AllValidTileCounts))] ImmutableDictionary<char, int> tileCounts)
+    {
+        var tileBag = new TileBag<char>(tileCounts);
+        var drawn   = tileBag.DrawX(Random.Shared, tileCounts.Values.Sum());
+
+        TestHelpers.AssertEquals(drawn.Count(), tileCounts.Values.Sum());
+
+        foreach (var (tile, expectedTileCount) in tileCounts)
+        {
+            var actualTileCount = drawn.Count(it => it == tile);
+            TestHelpers.AssertEquals(actualTileCount, expectedTileCount);
+        }
+    }
+
+    [Test]
+    public void DrawX_TooManyTiles([ValueSource(nameof(AllValidTileCounts))] ImmutableDictionary<char, int> tileCounts)
+    {
+        var tileBag            = new TileBag<char>(tileCounts);
+        var originalTotalTiles = tileBag.TotalTiles;
+        Assert.That(() => tileBag.DrawX(Random.Shared, tileBag.TotalTiles + 1), Throws.Exception);
+        TestHelpers.AssertEquals(tileBag.TotalTiles, originalTotalTiles);
+        Assert.That(tileBag.TotalTiles, Is.EqualTo(originalTotalTiles));
+    }
+}
