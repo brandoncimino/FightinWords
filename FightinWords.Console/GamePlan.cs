b73ce5d1f7fe5697bd676bc2686ad96318ef2be0
BCimino <bcimino@cccis.com>
2025-02-06 08:52:33 -0600

    Added `Validate` and made `TimeSpan` nullable

--- FightinWords.Console/GamePlan.cs
+++ FightinWords.Console/GamePlan.cs
@@ -9,8 +9,15 @@ public sealed record GamePlan
 {
     public required Language             Language          { get; init; }
     public required Word                 ProgenitorPool    { get; init; }
-    public required TimeSpan             TimeLimit         { get; init; }
+    public required TimeSpan?            TimeLimit         { get; init; } = null;
     public          SpectreFactory.Theme Theme             { get; init; } = new();
     public          int                  MinimumWordLength { get; init; } = 3;
     public          string?              RandomSeed        { get; init; } = null;
+
+    public static void Validate(GamePlan gamePlan)
+    {
+        Preconditions.Require(gamePlan.ProgenitorPool.Length > 0);
+        Preconditions.Require(gamePlan.TimeLimit is null || gamePlan.TimeLimit > TimeSpan.Zero);
+        Preconditions.Require(gamePlan.MinimumWordLength >= 0);
+    }
 }
