b06f4881cf6461b7945d4767c9dea62ffa1170b8
BCimino <bcimino@cccis.com>
2025-02-07 19:42:09 -0600

    Added GameAssistantDirector.cs

---
+++ FightinWords.Console/GameAssistantDirector.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console;
+
+namespace FightinWords.Console;
+
+/// <summary>
+/// Static methods that implement bits of logic for the <see cref="GameDirector"/>.
+/// </summary>
+public static class GameAssistantDirector
+{
+    public static GameReferee StartGame(GamePlanner planner, IAnsiConsole console, Random random)
+    {
+        var gamePlan        = planner.LockIn(random);
+        var sharedResources = SharedResources.Create(gamePlan, console);
+        var referee         = GameReferee.StartGame(sharedResources);
+        return referee;
+    }
+
+    public static (GamePlanner, FinalResults) EndGame(GameReferee referee)
+    {
+        return (new GamePlanner(), new FinalResults(referee.GetCurrentSubmissions()));
+    }
+}
